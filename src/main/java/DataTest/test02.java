797ee9adbef2c7790c3f3ffdc0f598867ac96e4c
pilipalaboy <2290649030@qq.com>
2020-05-20 16:57:34 +0800

    软件1.0版本内容提交

---
+++ src/main/java/DataTest/test02.java
@@ -0,0 +1,45 @@
+package DataTest;
+import com.csvreader.CsvReader;
+import com.csvreader.CsvWriter;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class test02 {
+    public static void main(String[] args) throws IOException {
+        String filePath = "D:\\资料\\安装资料\\任务\\jobs6.csv";
+        InputStream in = new FileInputStream(filePath);
+        CsvReader cr = new CsvReader(in, Charset.forName("utf-8"));
+
+        // 读表头
+        cr.readHeaders();
+        String filePath2 = "D:\\资料\\安装资料\\任务\\6.csv";
+        // 创建CSV写对象
+        CsvWriter cs = new CsvWriter(filePath2,',', Charset.forName("utf-8"));
+        //CsvWriter csvWriter = new CsvWriter(filePath);
+
+
+        while(cr.readRecord()) {
+            String RawRecord = cr.getRawRecord();
+            if(RawRecord.startsWith("company_financing_stage")){
+                continue;
+            }
+            int columnCount = cr.getColumnCount();
+            String[] s = new String[cr.getColumnCount()];
+            for (int i = 0; i <columnCount; i++) {
+                String str = cr.get(i);
+                Pattern p = Pattern.compile("\\s+|\t+|\n\r");
+                Matcher m = p.matcher(str);
+                s[i] = m.replaceAll("");
+
+
+            }
+            cs.writeRecord(s);
+        }
+
+        cs.close();
+    }
+}
